ef4fc4677b1cc0602c19b0474c86af857939045f
Milad Alghosi <javad.alghosi7904@gmail.com>
2022-05-14 12:59:35 +0430

    add favicon

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const favicon = document.querySelector("#favicon");
+
+const changeFaviconDefault = () => {
+  favicon.setAttribute("href", "images/1.png");
+};
+
+const changeFavicon = () => {
+  favicon.setAttribute("href", "images/2.jpg");
+};
